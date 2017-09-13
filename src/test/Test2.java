0444795d8b1360833811c7fcb132e6712927a5a6
dimaOrlovskiy <Orlovskiy.diman@gmail.com>
2017-09-13 16:21:14 -0400

    Exception in thread "main" java.lang.Error: Unresolved compilation problems:

--- src/test/Test2.java
+++ src/test/Test2.java
@@ -3,9 +3,9 @@ package test;
 public class Test2 {
 public static void main(String[] args) {
 
-	System.out.println("did it :))");
+	System.out.printl("did it :))");
 
-	System.out.println("Yeap ");
+	System.out.rintln("Yeap ");
 
 }
 }
