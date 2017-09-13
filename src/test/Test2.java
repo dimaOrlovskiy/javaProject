4e57394e0fbc084757c897e06f79831b5829c440
dimaOrlovskiy <Orlovskiy.diman@gmail.com>
2017-09-13 19:17:17 -0400

    Test2

--- src/test/Test2.java
+++ src/test/Test2.java
@@ -3,9 +3,9 @@ package test;
 public class Test2 {
 public static void main(String[] args) {
 
-	System.out.printl("did it :))");
+	System.out.println("did it :))");
 
-	System.out.rintln("Yeap ");
+	System.out.println("Yeap ");
 
 }
 }
